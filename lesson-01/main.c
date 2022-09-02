a008fc1f77a89ee27e4ebc36b54903b34d1c639b
Johannes Rebling <johannes.rebling@metergroup.com>
2022-09-02 15:59:46 +0200

    - first few lessons

--- lesson-01/main.c
+++ lesson-01/main.c
@@ -1,27 +1,19 @@
+#include <stdint.h>
+#include <stdbool.h>
 
 int main() {
-    int counter = 0;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
+  *((uint32_t *)0x400FE608U) = 0x20U; // enable clock to GPIOF
+  *((uint32_t *)0x40025400U) = 0x0EU; // enable digital I/O on PF1, PF2, PF3
+  *((uint32_t *)0x4002551CU) = 0x0EU; // enable LED on PF1, PF2, PF3
 
-    return 0;
+  while (true) {
+    *((uint32_t *)0x400253FCU) = 0x0EU; // turn on LED on PF1, PF2, PF3
+    for (int i = 0; i < 1000000; i++) {
+    }
+    *((uint32_t *)0x400253FCU) = 0x00U; // turn off LED on PF1, PF2, PF3
+    for (int i = 0; i < 1000000; i++) {
+    }
+  }
+
+  return 0;
 }
--- lesson-02/main.c
+++ lesson-02/main.c
@@ -1,15 +1,19 @@
+#include "intrinsics.h"
 
 int main() {
-    int counter = 0;
-    while (counter < 21) {
-        ++counter;
-        if ((counter & 1) != 0) {
-            /* do something when the counter is odd */
-        }
-        else {
-            /* do something when the counter is even */
-        }
+  int counter = 0;
+  while (counter < 21) {
+    ++counter;
+    if ((counter & 1) != 0) {
+      /* do something when the counter is odd */
+      __no_operation();
+      asm("NOP");
+    } else {
+      /* do something when the counter is even */
+      __iar_builtin_no_operation();
+      asm("NOP");
     }
+  }
 
-    return 0;
+  return 0;
 }
